9090663d43adbb2d4086d1a8a6c59f6de5895860
wahyu alfarisi <wahyualfarisi@gmail.com>
2020-08-12 15:02:19 +0700

    Updates

---
+++ precache-manifest.b1122fb74516e0c5ca6b9e0492c22fb6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18a74152df00f5a5c38ccd9ef6487b15",
+    "url": "/Burger-builder/index.html"
+  },
+  {
+    "revision": "0db2887a6f4ddcb3bd99",
+    "url": "/Burger-builder/static/css/main.807775ca.chunk.css"
+  },
+  {
+    "revision": "7f9911a375380a1f83d8",
+    "url": "/Burger-builder/static/js/2.bc859a20.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Burger-builder/static/js/2.bc859a20.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0db2887a6f4ddcb3bd99",
+    "url": "/Burger-builder/static/js/main.24099789.chunk.js"
+  },
+  {
+    "revision": "0df4043e8d5de35b87f2",
+    "url": "/Burger-builder/static/js/runtime-main.03510603.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/Burger-builder/static/media/burger-logo.b8503d26.png"
+  }
+]);
