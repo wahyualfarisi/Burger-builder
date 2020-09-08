1312bf5cf3c7b58efb9c7050f563643a4561301d
wahyu alfarisi <wahyualfarisi@gmail.com>
2020-09-08 14:32:42 +0700

    add test case navigationItems

---
+++ src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+import NavigationItems from './NavigationItems';
+import NavigationItem from './NavigationItem/NavigationItem';
+
+configure({adapter: new Adapter()});
+
+describe('<NavigationItems />', () => {
+    it('Should render two <NavigationItem /> elements if not authenticated', () => {
+        const wrapper = shallow(<NavigationItems />)
+        expect(wrapper.find(NavigationItem)).toHaveLength(0);
+    })
+
+
+    it('Should render three <NavigationItem /> elements if authenticated', () => {
+        const wrapper = shallow(<NavigationItems isAuth />)
+        expect(wrapper.find(NavigationItem)).toHaveLength(3);
+    })
+})
