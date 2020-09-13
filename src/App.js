c741039b1db91a1180f9c94600e84ba098bc9b47
wahyu alfarisi <wahyualfarisi@gmail.com>
2020-09-13 19:49:13 +0700

    using hook: on App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 import Layout from './hoc/Layout/Layout';
@@ -12,41 +12,41 @@ import * as actions from './store/actions/index';
 
 
 
-class App extends React.Component {
+const App = props => {
  
-  componentDidMount(){
-    this.props.onTryAutoSignUp();
-  }
+  useEffect( () => {
+    props.onTryAutoSignUp();
 
-  render(){
-    let routes = (
+  }, [])
+  
+  let routes = (
+    <Switch>
+      <Route path="/" exact component={BurgerBuilder} />
+      <Route path="/auth" component={Auth} />
+      <Redirect to="/" />
+    </Switch>
+  )
+
+  if(props.isAuthenticated){
+    routes = (
       <Switch>
         <Route path="/" exact component={BurgerBuilder} />
-        <Route path="/auth" component={Auth} />
+        <Route path="/checkout"  component={Checkout} />
+        <Route path="/orders" component={Orders} />
+        <Route path="/logout" component={Logout} />
         <Redirect to="/" />
       </Switch>
     )
-
-    if(this.props.isAuthenticated){
-      routes = (
-        <Switch>
-          <Route path="/" exact component={BurgerBuilder} />
-          <Route path="/checkout"  component={Checkout} />
-          <Route path="/orders" component={Orders} />
-          <Route path="/logout" component={Logout} />
-          <Redirect to="/" />
-        </Switch>
-      )
-    }
-    return (
-      <div>
-          <Layout>
-            {routes}
-          </Layout>
-      </div>
-    );
-
   }
+
+  return (
+    <div>
+        <Layout>
+          {routes}
+        </Layout>
+    </div>
+  );
+
 }
 
 const mapStateToProps = state => {
